32be7dbc211269617c53f13fbb376a1c690df930
13006601 <Leena@Lena>
2019-03-29 20:31:28 +0000

    new commit

--- src/queuemanager/ListNode.java
+++ src/queuemanager/ListNode.java
@@ -1,11 +1,18 @@
   package queuemanager;
 
-
+  /*
+   this is a node what holding data about item, item priority number and  
+  node link to the next node, nodes are using in the 
+  ordered and unorderd link lists.
+  */
 public class ListNode<T> {
     private final T item;
     public final int priority;
     public ListNode<T> next;
    
+    /*
+     creating an empty node with given parametrs.
+    */
     
     public ListNode (T item, int priority, ListNode<T> next)
     {
@@ -15,17 +22,23 @@ public class ListNode<T> {
         
     }
 
-    
+    /*
+     return item data
+    */
     public T getItem()
     {
         return item;
     }
-    
+    /*
+      return item priority number
+    */
     public int getPriotiy()
     {
         return priority;
     }
-    
+    /*
+     return node link
+    */
     public ListNode<T> getNext()
     {
         return next;
